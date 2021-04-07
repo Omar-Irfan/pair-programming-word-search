d5afc7cf5414b228af360985a10b9588f58861be
Omar Irfan <omar.irfan18@gmail.com>
2021-04-07 21:46:33 +0000

    finished assignment with Imtiaz

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,52 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("should return true if the word is present and smaller", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEIN')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present and smaller and vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ASYH')
+
+    assert.isTrue(result);
+  });
+  it("should return 'Incorrect input' if letters is an empty array", function() {
+    const result = wordSearch([], 'ASYH')
+
+    assert.deepEqual(result, "Incorrect input");
+  });
+  it("should return 'Incorrect input' if words is an empty string", function() {
+    const result = wordSearch(['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'], '')
+
+    assert.strictEqual(result, "Incorrect input");
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//Paired Omar and Imtiaz
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+    matrix = matrix[0].map((element, index) => matrix.map(row => row[index]));
+    return matrix
+  };
+  
+  let result = false;
+  let flag = false;
+  
+  const wordSearch = (letters, word) => {
+    if (letters.length === 0 || letters === undefined || word === '' || word === undefined || !Array.isArray(letters) || (typeof word) !== "string") {
+        return "Incorrect input";
+    } else {
+  
+      const horizontalJoin = letters.map(ls => ls.join(''))
+      for (l of horizontalJoin) {
+        if (l.includes(word)) {
+          result = true;
+        } else if (flag === false) {
+          flag = true;
+          wordSearch(transpose(letters), word);
+        }
+      }
+      return result;
+    }
+  }
+
+  module.exports = wordSearch;
